e39d559f438ba3775d0371f1431cba5051f0bb23
mhabib04 <mhabib180404@gmail.com>
2024-04-27 19:28:49 +0700

    done

--- app/src/main/java/com/example/uts/api/ApiConfig.java
+++ app/src/main/java/com/example/uts/api/ApiConfig.java
@@ -1,16 +1,26 @@
 package com.example.uts.api;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiConfig {
+    @NonNull
     public static ApiService getApiService() {
+
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com/").addConverterFactory(GsonConverterFactory.create()).client(client).build();
-        return retrofit.create(ApiService.class);
+        ApiService apiService = retrofit.create(ApiService.class);
+        Log.d("ApiConfig", "ApiService: " + apiService);
+
+        return apiService;
     }
 }
 
--- app/src/main/java/com/example/uts/api/ApiService.java
+++ app/src/main/java/com/example/uts/api/ApiService.java
@@ -5,14 +5,17 @@ import com.example.uts.data.User;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
+    @Headers({"Authorization: token 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"})
     @GET("search/users")
     Call<SearchUserResponse> searchUsers(@Query("q") String query);
 
+    @Headers({"Authorization: token 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"})
     @GET("users/{username}")
     Call<User> getUser(@Path("username") String username);
 }
