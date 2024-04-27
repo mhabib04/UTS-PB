82501897d58e3cd67e83f009da7438c54baa6a34
mhabib04 <mhabib180404@gmail.com>
2024-04-27 20:51:11 +0700

    done

--- app/src/main/java/com/example/uts/api/ApiService.java
+++ app/src/main/java/com/example/uts/api/ApiService.java
@@ -5,16 +5,15 @@ import com.example.uts.data.User;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
-    @Headers({"Authorization: token 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"})
+    //@Headers({"Authorization: token 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"})
     @GET("search/users")
     Call<SearchUserResponse> searchUsers(@Query("q") String query);
 
-    @Headers({"Authorization: token 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"})
+    //@Headers({"Authorization: token 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"})
     @GET("users/{username}")
     Call<User> getUser(@Path("username") String username);
 }
